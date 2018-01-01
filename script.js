86f0bff7b1817dbedec5fcf6a010460d9b82cf6a
xuanhoa66 <xuanhoa66@gmail.com>
2018-01-01 15:47:08 +0700

    ttt

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
         html2canvas(form).then( function(canvas){
              var a = document.createElement('a');
         // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
-            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            a.href = canvas.toDataURL("image/jpeg");
             a.download = 'somefilename.jpg';
             a.click();
 
